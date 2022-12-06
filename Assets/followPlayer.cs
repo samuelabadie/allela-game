59a7f0a4168ee21d535a624e40113eafec6e2766
samuelabadie <91666297+samuelabadie@users.noreply.github.com>
2022-12-06 10:07:07 +0100

    V1

--- Assets/followPlayer.cs
+++ Assets/followPlayer.cs
@@ -16,7 +16,6 @@ public class followPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      
         transform.position = player.transform.position + new Vector3(0, 1, -5);
     }
 }
